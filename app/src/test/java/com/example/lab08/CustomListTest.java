c9d1309c929f3745bb683cd3ed9db76be34b820a
Greg Madro <gmadro@ualberta.ca>
2025-10-31 16:56:59 -0600

    Added a Failing Test for countCities()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -30,4 +30,19 @@ public class CustomListTest {
         // Edmonton should still exist
         assertTrue(list.hasCity(edmonton));
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+
+        assertEquals(0, list.countCities());
+
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
+    }
 }
